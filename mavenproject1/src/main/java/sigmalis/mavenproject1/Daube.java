1436c880145921511da3de2627e06ed50c684361
LucasDuval <lucas.duval@sigmalis.ch>
2019-11-21 09:21:34 +0100

    init2

---
+++ mavenproject1/src/main/java/sigmalis/mavenproject1/Daube.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sigmalis.mavenproject1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Daube {
+	private int monAttribut;
+
+	public int getMonAttribut() {
+		return monAttribut;
+	}
+
+	public void setMonAttribut(int monAttribut) {
+		this.monAttribut = monAttribut;
+	}
+}
