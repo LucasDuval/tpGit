c9f976570c87fdbdd2767c6240e0a0e0ee049707
LucasDuval <lucas.duval@sigmalis.ch>
2019-11-21 08:50:30 +0100

    Add attribute

--- src/MaClasse.java
+++ src/MaClasse.java
@@ -1,4 +1,19 @@
 
 public class MaClasse {
-
+	public int attribut1;
+	public boolean attribut2;
+	
+	public int getAttribut1() {
+		return attribut1;
+	}
+	public void setAttribut1(int attribut1) {
+		this.attribut1 = attribut1;
+	}
+	public boolean isAttribut2() {
+		return attribut2;
+	}
+	public void setAttribut2(boolean attribut2) {
+		this.attribut2 = attribut2;
+	}
+	
 }
