dbcc93cb54f352a899d34d555430b9b3a86e009f
LucasDuval <lucas.duval@sigmalis.ch>
2019-11-21 09:32:20 +0100

    init2

---
+++ mavenproject2/src/main/java/sigmalis/mavenproject2/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sigmalis.mavenproject2;
+
+/**
+ *
+ * @author Admin
+ */
+public class NewClass {
+	int monInt;
+
+	public int getMonInt() {
+		return monInt;
+	}
+
+	public void setMonInt(int monInt) {
+		this.monInt = monInt;
+	}
+	
+}
